ba468ae955936d81f9214a65e9b30b003f653632
André Deitos <44508803+andred88@users.noreply.github.com>
2025-12-08 21:55:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const jwt = require("jsonwebtoken")
 const { exec } = require("child_process")
 const util = require("util")
 const execPromise = util.promisify(exec)
+const path = require('path');
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+// Rotas para páginas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
 
 const app = express()
 app.use(express.json())
